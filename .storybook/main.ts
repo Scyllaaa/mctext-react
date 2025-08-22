c0eb8594598824dc3715510b0ed0a4a6e4748ab4
Rhys <rhys.smith@ghyston.com>
2025-08-22 14:37:24 +0100

    updated to react 19

---
+++ .storybook/main.ts
@@ -0,0 +1,7 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+    stories : ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+    framework: '@storybook/react-vite',
+}
+
